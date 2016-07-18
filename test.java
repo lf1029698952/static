d55cf720fa69a5c5564b4d8ea26821bacaa847c5
lf1029698952 <3659384@163.com>
2016-07-18 15:04:15 +0800

    hadoop-study test

---
+++ test.java
@@ -0,0 +1,11 @@
+/**
+ * github repository
+ * @author lifeng
+ * hadoop-study test
+ */
+
+ Class Test {
+	public static void public static void main(String[] args) {
+		System.out.println("hadoop-study test");
+	}
+ }
